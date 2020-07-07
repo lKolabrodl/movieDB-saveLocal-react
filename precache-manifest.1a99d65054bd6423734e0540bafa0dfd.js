d1b196452c3870b80dc0b7b6f30abc60a00d3096
Bril Mikhail <Kolabrod@mail.ru>
2020-07-07 17:00:08 +0300

    Updates

---
+++ precache-manifest.1a99d65054bd6423734e0540bafa0dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3f1f46c48aac357733c2cb9da14540",
+    "url": "/movieDB-saveLocal-react/index.html"
+  },
+  {
+    "revision": "35da58ac42e909d48f77",
+    "url": "/movieDB-saveLocal-react/static/css/main.c28d4196.chunk.css"
+  },
+  {
+    "revision": "54232863f04d160afb55",
+    "url": "/movieDB-saveLocal-react/static/js/2.159e3bb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieDB-saveLocal-react/static/js/2.159e3bb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35da58ac42e909d48f77",
+    "url": "/movieDB-saveLocal-react/static/js/main.e789863d.chunk.js"
+  },
+  {
+    "revision": "62f6dba0c9afe62f5a5b",
+    "url": "/movieDB-saveLocal-react/static/js/runtime-main.74cdbfff.js"
+  }
+]);
